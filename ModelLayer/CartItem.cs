785ae8d8849f1274d100611e11ef41d8414e5fe7
asmitamthorat <asmita26296@gmail.com>
2021-01-25 06:58:10 +0530

    [asmita]ADD:added userController for registration and login

---
+++ ModelLayer/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class CartItem
+    {
+        public int cartItem_id { get; set; }
+        public int bookId { get; set; }
+
+        public int quantity { get; set; }
+        public int price { get; set; }
+    }
+}
